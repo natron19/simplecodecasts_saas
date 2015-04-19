cdeec3bbea0326ff8803697b7c41eada5742b8d0
Nathan R <nathan19@hotmail.com>
2015-04-18 22:45:36 -0700

    added mailer

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,13 @@
+class ContactMailer < ActionMailer::Base 
+  
+  default to: 'nathan19@hotmail.com' 
+
+  def contact_email(name, email, body) 
+    @name = name
+    @email = email
+    @body = body 
+
+    mail(from: email, subject: 'Contact form message')
+  end
+
+end 
