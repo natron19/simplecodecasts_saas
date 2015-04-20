23caef6ed485cad89e86d08887e1def997a84420
Nathan R <nathan19@hotmail.com>
2015-04-20 14:29:32 -0700

    added profile new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,10 @@
 class ProfilesController < ApplicationController
 
+  def new 
+    #form where user can fill out their own profile 
+    @user = User.find(params[:user_id]) 
+    @profile = @user.build_profile 
+  end 
+
 
 end 
